3cfe2cd471fc880f4fffe70639eaad276fce3fc3
Cheng Hao Ke <102256006c@gmail.com>
2020-12-19 20:50:06 -0600

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -6,7 +6,7 @@ const fetcher = (variables, token) => {
   return request(
     {
       query: 
-      query userInfo($login: "ChengHaoKe") {
+      query userInfo($login: String!) {
         user(login: $login) {
           # fetch only owner repos & not forks
           repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
