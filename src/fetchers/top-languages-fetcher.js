a9b64c84dc806c652f8b7b18fbeae39c3d93f7e7
Cheng Hao Ke <102256006c@gmail.com>
2020-12-24 17:58:42 -0600

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -9,7 +9,7 @@ const fetcher = (variables, token) => {
       query userInfo($login: String!) {
         user(login: $login) {
           # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 1) {
+          repositories(ownerAffiliations: OWNER, isFork: false, first: 2) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
